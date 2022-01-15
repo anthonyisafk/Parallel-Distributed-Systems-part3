6c34d9c582291030504b310c67cf1e6879c5f7b9
anthonyisafk <tonytonytony555@live.com>
2022-01-15 23:47:49 +0000

    Finish(?) sequential implementation

--- v0_sequential.c
+++ v0_sequential.c
@@ -14,31 +14,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N 5
-
 
 void print_model(int **model, int n) {
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
-      printf("%d ", model[i][j]);
+      printf("%d  ", model[i][j]);
     }
     printf("\n");
   }
 }
 
 
+// Implement the sign function with a node's neighbors.
+int sign(int self, int *neighbours, int neighbours_n) {
+  int sum = self;
+  for (int i = 0; i < neighbours_n; i++) {
+    sum += neighbours[i];
+  }
+
+  if (sum > 0) {
+    return 1;
+  }
+  else if (sum < 0) {
+    return -1;
+  } else {
+    return 0; /* never happens, added for mathematic completeness */
+  }
+}
+
+
+/**
+ * Instead of using if's or implementing a struct of any sort, we will be using this.
+ * Rolls the array into itself. Index `size` points to `0` and index `-1` points to `n-1`
+ * Ask for the indices and the size of the model. 
+ */
+int get_model(int **model, int i, int j, int n) {
+  return model[(i + n) % n][(j + n) % n];
+}
+
+
 int main (int argc, char **argv) {
+  if (argc != 3) {
+    printf("Usage: ./v0.o N K, where N is size and K is iterations\n");
+    return -1;
+  }
+  int N = atoi(argv[1]);
+  int K = atoi(argv[2]);
 
   int **model = (int **) malloc(N * sizeof(int *));
   for (int i = 0; i < N; i++){
     model[i] = (int *) calloc(N, sizeof(int));
   }
 
-  // The spins are initializes as 0's. We set them to +1 or -1 using a uniform distribution.
+  // The spins are initialized as 0's. We set them to +1 or -1 using a uniform distribution.
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
-      model[i][j] = (rand() > (int)RAND_MAX / 2) ? -1 : 1;
+      model[i][j] = (rand() > RAND_MAX / 2) ? -1 : 1;
     }
+  }
+  printf("MODEL BEFORE FIRST ITERATION\n");
+  print_model(model, N);
+
+  /* ---------- START SIMULATION ---------- */
+  int neighbours[4];
+
+  for (int iter = 0; iter < K; iter++) {
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        neighbours[0] = get_model(model, i, j + 1, N);
+        neighbours[1] = get_model(model, i, j - 1, N);
+        neighbours[2] = get_model(model, i + 1, j, N);
+        neighbours[3] = get_model(model, i - 1, j, N);
+
+        model[i][j] = sign(model[i][j], neighbours, 4);
+      }
+    }
+
+    printf("\nMODEL ON ITERATION iter = %d\n", iter);
+    print_model(model, N);
   }
 
   return 0;
